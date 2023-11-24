c864feab3cb54f8d8c7fd0d9f1307555ca2e03e1
aerlbaum3 <121773976+aerlbaum3@users.noreply.github.com>
2023-11-23 23:40:53 -0500

    Add files via upload

--- TicTacToeTest.java
+++ TicTacToeTest.java
@@ -10,10 +10,30 @@ class TicTacToeTest {
         TicTacToeGame game = new TicTacToeGame();
         assertEquals(1, game.getTurn());
     }
-//	public void testDisplayWinner() {
-//        TicTacToeGame game = new TicTacToeGame();
-//        assertEquals("X won.", game.displayWinner("X", true));
-//    }
+	@Test
+	public void testCheckWinnerWhenPassInAnX() {
+        TicTacToeGame game = new TicTacToeGame();
+        assertEquals("X won.", game.displayWinner("X", true));
+    }
+	
+	@Test
+	public void testCheckWinnerWhenPassInAnO() {
+		TicTacToeGame game = new TicTacToeGame();
+        assertEquals("O won.", game.displayWinner("O", true));
+    }
+	public void testCheckWinnerWhenPassInAnAFalseForTheWinner() {
+		TicTacToeGame game = new TicTacToeGame();
+        assertEquals("Draw", game.displayWinner("O", false));
+    }
+	
+	@Test
+	public void testForValidMove() {
+		Board board = new Board();
+		assertFalse(board.isValidMove(10, board.X_SPOT) );
+		
+	}
+	
+	
 }
 
 
